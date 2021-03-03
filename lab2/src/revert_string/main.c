f88cac99eb3dff7806ca33a2b82b30c3b58a5921
valeuya <lerayablockina@mail.ru>
2021-03-03 15:35:29 +0000

    добавлен main.c main.o revert_string.c librevert.so revert_string.o libstatic.a revert_string.o main swap.c

--- lab2/src/revert_string/main.c
+++ lab2/src/revert_string/main.c
@@ -6,19 +6,20 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc != 2)
-	{
-		printf("Usage: %s string_to_revert\n", argv[0]);
-		return -1;
-	}
+    if (argc != 2)/* argc должен быть 2, для правильного выполнения */
+    {
+        printf("Usage: %s string_to_revert\n", argv[0]);/* Мы печатаем argv [0], предполагая, что это имя программы */
+        return -1;
+    }
 
-	char *reverted_str = malloc(sizeof(char) * (strlen(argv[1]) + 1));
-	strcpy(reverted_str, argv[1]);
+    char *reverted_str = malloc(sizeof(char) * (strlen(argv[1]) + 1));/*выделяем память для строки размером
+    в данном случае sizeof (char) 1 байт в n раз больше (длина строки argv[1]+1)*/
 
-	RevertString(reverted_str);
+    strcpy(reverted_str, argv[1]);/*копируем изначальную строку в выделенный бок памяти*/
 
-	printf("Reverted: %s\n", reverted_str);
-	free(reverted_str);
-	return 0;
+    RevertString(reverted_str); /*Функция для переворачивания строки*/
+
+    printf("Reverted: %s\n", reverted_str); /*печатаем результат*/
+    free(reverted_str); /*освобождаем память, выделенную в куче*/
+    return 0;
 }
-
--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,18 @@
 #include "revert_string.h"
+#include <string.h>
 
 void RevertString(char *str)
 {
-	// your code here
+    int i, j;
+    char c;
+
+    for (i = 0, j = strlen(str)-1; i < j; i++, j--) 
+    {
+        c = *(str+i);
+        *(str+i) = *(str+j);
+        *(str+j) = c;
+    }
+    
 }
+
 
